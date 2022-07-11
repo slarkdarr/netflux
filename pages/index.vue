72c75ecc607809cc147743acf1005fd24da478b0
slarkdarr <daffaresyaly@gmail.com>
2022-07-11 14:53:21 +0700

    Add search feature

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,63 @@
     <!-- Header -->
     <Header />
 
+    <!-- Search -->
+    <div class="container search">
+      <input
+        v-model.lazy="searchInput"
+        type="text"
+        placeholder="Search"
+        @keyup.enter="$fetch"
+      />
+      <button v-show="searchInput !== ''" class="button" @click="clearSearch">
+        Clear Search
+      </button>
+    </div>
+
     <!-- Movies -->
     <div class="container movies">
-      <div id="movie-grid" class="movies-grid">
+      <!-- Searched Movies -->
+      <div v-if="searchInput !== ''" id="movie-grid" class="movies-grid">
+        <div
+          v-for="(movie, index) in searchedMovies"
+          :key="index"
+          class="movie"
+        >
+          <div class="movie-img">
+            <img
+              :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
+              alt=""
+            />
+            <p class="review">{{ movie.vote_average }}</p>
+            <p class="overview">{{ movie.overview }}</p>
+          </div>
+          <div class="info">
+            <p class="title">
+              {{ movie.title.slice(0, 25) }}
+              <span v-if="movie.title.length > 25"></span>
+            </p>
+            <p class="release">
+              Released:
+              {{
+                new Date(movie.release_date).toLocaleString('en-US', {
+                  month: 'long',
+                  day: 'numeric',
+                  year: 'numeric',
+                })
+              }}
+            </p>
+            <NuxtLink
+              class="button button-light"
+              :to="{ name: 'movies-movieid', params: { movieid: movie.id } }"
+            >
+              Get More Info
+            </NuxtLink>
+          </div>
+        </div>
+      </div>
+
+      <!-- Popular Movies -->
+      <div v-else id="movie-grid" class="movies-grid">
         <div v-for="(movie, index) in movies" :key="index" class="movie">
           <div class="movie-img">
             <img
@@ -52,10 +106,17 @@ export default {
   data() {
     return {
       movies: [],
+      searchedMovies: [],
+      searchInput: '',
     }
   },
   async fetch() {
-    await this.getMovies()
+    if (this.searchInput === '') {
+      await this.getMovies()
+      return
+    }
+
+    await this.searchMovies()
   },
   methods: {
     async getMovies() {
@@ -66,6 +127,21 @@ export default {
       result.data.results.forEach((movie) => {
         this.movies.push(movie)
       })
+    },
+
+    async searchMovies() {
+      const data = axios.get(
+        `https://api.themoviedb.org/3/search/movie?api_key=ffecda3126f77eae77aaa1b33b970b17&language=en-US&page=1&query=${this.searchInput}`
+      )
+      const result = await data
+      result.data.results.forEach((movie) => {
+        this.searchedMovies.push(movie)
+      })
+    },
+
+    clearSearch() {
+      this.searchInput = ''
+      this.searchedMovies = []
     },
   },
 }
