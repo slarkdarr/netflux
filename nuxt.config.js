4690e94c545f0dc56e12a99c0ece8c93f15885ab
daffaresyaly <daffa.resyaly@swiperxapp.com>
2022-07-27 12:00:39 +0700

    Split scss into files in assets

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,11 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/assets/default.scss'],
+  css: [
+    '@/assets/default.scss',
+    '@/assets/scss/header.scss',
+    '@/assets/scss/index.scss',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
